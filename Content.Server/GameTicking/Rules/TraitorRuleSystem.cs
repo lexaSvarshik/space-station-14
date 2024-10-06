ee99aaf814f4406fa1796b2cbb2ed273d36bad87
Svarshik <lexasvarshik666@gmail.com>
2024-10-06 20:15:42 +0500

    Revert "revert of wizden discounts"
    This reverts commit 4b1e7bd6428a4a670142d2a47e822951cfa89b90.

--- Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
+++ Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
@@ -111,8 +111,6 @@ public sealed class TraitorRuleSystem : GameRuleSystem<TraitorRuleComponent>
             // creadth: we need to create uplink for the antag.
             // PDA should be in place already
             var pda = _uplink.FindUplinkTarget(traitor);
-            if (pda == null || !_uplink.AddUplink(traitor, startingBalance))
-                return false;
 
             //ss220 fix no codewords for traitor w/o pda start
             if (pda != null && _uplink.AddUplink(traitor, startingBalance, giveDiscounts: true))
