9d90b330c57c7b62e6d8d58ca03cc8f887197cae
ReeZer2 <63300653+ReeZer2@users.noreply.github.com>
2025-06-01 12:49:42 +0300

    FIX: Drop Shield while on cooldown (#2980)
    * FIX: Drop Shield while on cooldown
    
    * tralalero tralala

--- Content.Shared/Blocking/BlockingSystem.cs
+++ Content.Shared/Blocking/BlockingSystem.cs
@@ -20,6 +20,9 @@ using Robust.Shared.Physics.Systems;
 using Robust.Shared.Utility;
 using Content.Shared.Weapons.Reflect;
 using Content.Shared.Inventory.Events;
+using Content.Shared.Throwing;
+using Robust.Shared.Containers;
+using Robust.Shared.Timing;
 
 namespace Content.Shared.Blocking;
 
@@ -34,6 +37,7 @@ public sealed partial class BlockingSystem : EntitySystem
     [Dependency] private readonly EntityLookupSystem _lookup = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
     [Dependency] private readonly ExamineSystemShared _examine = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
 
     public override void Initialize()
     {
@@ -58,7 +62,36 @@ public sealed partial class BlockingSystem : EntitySystem
         SubscribeLocalEvent<BlockingComponent, MapInitEvent>(OnMapInit);
 
         SubscribeLocalEvent<BlockingComponent, ItemToggledEvent>(OnToggleItem);
+
+        //ss220 fix drop shields start
+        SubscribeLocalEvent<BlockingComponent, ThrowItemAttemptEvent>(OnThrowAttempt);
+        SubscribeLocalEvent<BlockingComponent, ContainerGettingRemovedAttemptEvent>(OnDropAttempt);
+        //ss220 fix drop shields end
     }
+
+    //ss220 fix drop shields start
+    private void OnDropAttempt(Entity<BlockingComponent> ent, ref ContainerGettingRemovedAttemptEvent args)
+    {
+        if (IsDropBlocked(ent))
+            args.Cancel();
+    }
+
+    private void OnThrowAttempt(Entity<BlockingComponent> ent, ref ThrowItemAttemptEvent args)
+    {
+        if (IsDropBlocked(ent))
+            args.Cancelled = false;
+    }
+
+    private bool IsDropBlocked(Entity<BlockingComponent> ent)
+    {
+        var action = ent.Comp.BlockingToggleActionEntity;
+
+        if (action == null || !TryComp<InstantActionComponent>(action.Value, out var actionComponent))
+            return false;
+
+        return _gameTiming.CurTime <= actionComponent.Cooldown?.End;
+    }
+    //ss220 fix drop shields end
 
     //ss220 raise shield activated fix start
     private void OnToggleItem(Entity<BlockingComponent> ent, ref ItemToggledEvent args)
